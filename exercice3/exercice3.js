ea3e5e96d6fe1c5343b9abb3d1b524b60ef1b580
Lyz03 <lizoe@netcourrier.com>
2021-11-10 15:01:48 +0100

    part 3

--- exercice3/exercice3.js
+++ exercice3/exercice3.js
@@ -1,10 +1,20 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
+let x = 1;
+let y = 1;
 
-if()
-{
 
+if (x === y) {
+    document.getElementById('monDiv').innerHTML = "La variable x est strictement égale à la variable y";
+}
+else {
+    document.getElementById('monDiv').innerHTML = "La variable x n'est pas strictement égale à la variable y";
 }
 
-{
+x += 3
 
+if (x === y) {
+    document.getElementById('monDiv2').innerHTML = "La variable x est strictement égale à la variable y";
+}
+else {
+    document.getElementById('monDiv2').innerHTML = "La variable x n'est pas strictement égale à la variable y";
 }
