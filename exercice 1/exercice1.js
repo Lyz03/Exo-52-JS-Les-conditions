f455e80a322f615359ec3f65cf830928fa30e161
Lyz03 <lizoe@netcourrier.com>
2021-11-10 14:52:23 +0100

    part 1

--- exercice 1/exercice1.js
+++ exercice 1/exercice1.js
@@ -1,6 +1,12 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
-if x > y
-    document.getElementById('monDiv').innerHTML = "La variable x est supérieure à la variable y";
+let x = 5;
+let y = 8;
 
+if (x > y) {
+    document.getElementById('monDiv').innerHTML = "La variable x est supérieure à la variable y";
+}
+else {
+    document.getElementById('monDiv').innerHTML = "La variable y est supérieure à la variable x";
+}
 
